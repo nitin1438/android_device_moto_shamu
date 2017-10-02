7c6885f700e0a6db3162609196f10506cdc9ab66
elektroschmock <elektroschmock78@googlemail.com>
2017-10-02 02:01:56 -0400

    Compress the recovery ramdisk with LZMA
    Change-Id: I8188392431925a93822401e28697f68b632b06a6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,5 +146,8 @@ TARGET_FS_CONFIG_GEN += device/moto/shamu/config.fs
 
 -include vendor/moto/shamu/BoardConfigVendor.mk
 
+# Recovery
+LZMA_RAMDISK_TARGETS := recovery
+
 # Enable workaround for slow rom flash
 BOARD_SUPPRESS_SECURE_ERASE := true
